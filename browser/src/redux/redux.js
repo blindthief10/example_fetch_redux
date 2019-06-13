ac07900350e4890b0ec04767fed2dc66e27018b0
blindthief10 <kostasdiakogiannis23@gmail.com>
2019-06-13 11:51:52 +0200

    example redux fetch

--- browser/src/redux/redux.js
+++ browser/src/redux/redux.js
@@ -1,29 +1,54 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-const initialState = {messageToBeShown: 'Welcome Jake'};
+const initialState = {messageToBeShown: ''};
 
 const reduxReducer = (state = initialState, action) => {
   const copyOfState = {...state};
 
   switch(action.type){
     case 'FETCHEXAMPLE':
-      copyOfState.messageToBeShown = 'State has changed';
+      copyOfState.messageToBeShown = action.payload.message;
+      return copyOfState;
+    case 'ERRORCASE':
+      copyOfState.messageToBeShown = action.payload.message;
       return copyOfState;
     default:
       return copyOfState;
   }
 }
 
+const putTheMessageThere = daniel => {
+  return {
+    type: 'FETCHEXAMPLE',
+    payload: daniel
+  }
+}
+
+const badRequest = error => {
+  return {
+    type: 'ERRORCASE',
+    payload: error
+  }
+}
+
 export const fetchFromExpress = () => {
   return function(dispatch) {
-    fetch('/example')
+    fetch('/doesnotexist')
     .then(res => {
-      console.log(res);
-      return res.json();
+      if (res.status >= 400 && res.status < 500) {
+        throw new Error('You sack already!');
+      }else {
+        return res.json();
+      }
     })
-    .then(messageObject => console.log(messageObject))
-    .catch(err => console.error(err))
+    .then(messageObject => {
+      console.log(messageObject);
+      dispatch(putTheMessageThere(messageObject));
+    })
+    .catch(err => {
+      dispatch(badRequest(err))
+    })
   }
 }
 
